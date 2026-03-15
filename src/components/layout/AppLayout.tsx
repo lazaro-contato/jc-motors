2074adfd5604b7da4c3aae1ad5ab68c83124506d
José Lázaro <contato.jlazaro@gmail.com>
2026-03-14 21:04:00 -0300

    feat: add padding in medium pages

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -9,7 +9,7 @@ export default function AppLayout() {
       <Sidebar />
       <div className="flex flex-1 flex-col overflow-hidden">
         <Header />
-        <main className="flex-1 overflow-auto p-6">
+        <main className="flex-1 overflow-auto p-4 md:p-6">
           <Outlet />
         </main>
       </div>
